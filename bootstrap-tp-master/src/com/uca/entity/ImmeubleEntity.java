20f2ab48f1fbefae3414f8429ecb18f33f23f484
Th1b0lt <thibmure@orange.fr>
2024-04-10 09:12:10 +0200

    première classe

--- bootstrap-tp-master/src/com/uca/entity/ImmeubleEntity.java
+++ bootstrap-tp-master/src/com/uca/entity/ImmeubleEntity.java
@@ -4,8 +4,31 @@ public class ImmeubleEntity {
     private final int idImmeuble;
     private String nom;
     private SyndicatEntity syndicat;
-    private ArrayList<Appartement> appartements;
+    private ArrayList<AppartementEntity> appartements;
+    
+    public ImmeubleEntity(int idImmeuble){
+        this.idImmeuble=idImmeuble;
+        this.appartements=new ArrayList<AppartementEntity>;
+    }
 
+    //Liste des getters
+    public int getIdImmeuble(){
+        return this.idImmeuble;
+    }
+
+    public String getNom(){
+        return this.nom;
+    }
+
+    public SyndicatEntity getSyndicat(){
+        return this.syndicat;
+    }
+
+    public ArrayList<AppartementEntity> getAppartements(){
+        return this.appartements;
+    }
+    // Liste des setters
+   
 
 
 }
