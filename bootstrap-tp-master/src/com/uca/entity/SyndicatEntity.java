830d0ab63c6c6ef22d89dd672d3737fd69317c26
Th1b0lt <thibmure@orange.fr>
2024-04-10 08:24:20 +0200

    première classe

--- bootstrap-tp-master/src/com/uca/entity/SyndicatEntity.java
+++ bootstrap-tp-master/src/com/uca/entity/SyndicatEntity.java
@@ -1,5 +1,55 @@
 package src.com.uca.entity;
 
 public class SyndicatEntity {
-    
+    private final int idSyndicat;
+    private String name;
+    private String adresse;
+    private String personneReference;
+    private int numeroDeTelephone;
+    private String adresseEmail;
+
+    public SyndicatEntity(int idSyndicat){
+        this.idSyndicat=idSyndicat;
+    }
+    //Liste des getteur
+    public String getName(){
+        return this.name;
+    }
+    public String getAdresse(){
+        return this.adresse;
+    }
+
+    public String getPersonneReference(){
+        return this.personneReference;
+    }
+
+    public int getNumeroDeTelephone(){
+        return this.numeroDeTelephone;
+    }
+
+    public String getAdresseEmail(){
+        return this.adresseEmail;
+    }
+
+    //Liste des Setteur
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public void setAdresse(String adresse){
+        this.adresse=adresse;
+    }
+
+    public void setPersonneReference(String personne){
+        this.personneReference=personne;
+    }
+
+    public void setNumeroDeTelephone(String numero){
+        this.numeroDeTelephone=numero;
+    }
+
+    public void adresseEmail(String email){
+        this.adresseEmail=email;
+    }
+
 }
