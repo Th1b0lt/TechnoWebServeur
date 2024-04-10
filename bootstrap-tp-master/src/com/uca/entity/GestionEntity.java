3a141731512bfdeccabc27f46f4e8bc9064bb618
Th1b0lt <thibmure@orange.fr>
2024-04-10 09:39:16 +0200

    première classe

---
+++ bootstrap-tp-master/src/com/uca/entity/GestionEntity.java
@@ -0,0 +1,19 @@
+package src.com.uca.entity;
+
+public class GestionEntity{
+    private ArrayList<ImmeubleEntity> immeubles;
+    public GestionEntity(){
+        this.immeubles=new ArrayList<ImmeubleEntity>();
+    }
+    public ArrayList<ImmeubleEntity> getImmeubles(){
+        return this.immeubles;
+    }
+    public void ajouteImmeuble(ImmeubleEntity immeuble){
+        if (! this.immeubles.contains(immeuble)){
+            this.immeubles.add(immeuble);
+        }
+    }
+    public void supprimeImmeuble(ImmeubleEntity immeuble){
+        this.immeubles.remove(immeuble);
+    }
+}
